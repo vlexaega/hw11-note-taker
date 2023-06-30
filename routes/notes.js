2bb5619d086d7e659815e68b6674c449eff05e1b
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-06-30 09:14:43 -0400

    adds delete route to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -35,5 +35,22 @@ notes.post("/", (req, res) => {
 });
 
 //add delete route
+notes.delete("/:id", (req, res) => {
+    readFromFile('./db/db.json').then((data) => {
+        const notesData = JSON.parse(data);
+        const noteIndex = notesData.findIndex((note) => note.id === req.params.id);
+        if (noteIndex !== -1) {
+            notesData.splice(noteIndex, 1);
+            writeToFile('./db/db.json', JSON.stringify(notesData, null, 2)).then(() => {
+                res.json({ success: 'Note deleted successfully' });
+            }).catch((err) => {
+                res.status(500).json({ error: 'Failed to delete note' });
+            });
+        } else {
+            res.status(404).json({ error: 'Note not found' });
+        }
+    });
+});
+
 
 module.exports = notes;
