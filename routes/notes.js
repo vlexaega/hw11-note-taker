f2798a7f7f0f8ab4cb8284497a3ab6f85f25b936
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-06-30 08:52:40 -0400

    this updates the get route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,15 @@ const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils
 const { v4: uuidv4 } = require('uuid');
 
 notes.get("/", (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => {
+        const notesData = JSON.parse(data);
+        const note = notesData.find((note) => note.id === req.params.id);
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).json({ error: 'Note not found' });
+        }
+    });
 });
 
 //get notes by ID
@@ -19,7 +27,7 @@ notes.post("/", (req, res) => {
             text,
             id: uuidv4(),
         }
-        readAndAppend(newTask, 'db/db.json')
+        readAndAppend(newTask, './db/db.json')
         res.json('Task added Successfully!')
     } else {
         res.error('Error in posting task')
--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const path = require('path');
 const { clog } = require('./middleware/clog')
 const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
