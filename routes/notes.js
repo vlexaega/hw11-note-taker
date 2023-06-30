30ba859406625d094b13dd6986b3fa046fad5bd8
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-06-30 08:13:39 -0400

    committing before switching

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+notes.get('/', (req, res) => {
+    readFromFile.apply('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+//get notes by ID
+notes.get('/:id', (req, res) => {
+    res.json(JSON.parse(data)[req.params.id]);
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if(req.body){
+        const newTask = {
+            title,
+            text,
+            id: uuidv4(),
+        }
+        readAndAppend(newTask, 'db/db.json')
+        res.json('Task added Successfully!')
+    } else {
+        res.error('Error in posting task')
+    }
+});
+
+//add delete route
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
